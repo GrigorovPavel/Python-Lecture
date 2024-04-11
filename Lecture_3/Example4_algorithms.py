81ed14f1b1d1c20d13a8fd5ec9ea133ed962a4ef
Pavel <grigorov_ps@mail.ru>
2024-04-12 00:04:16 +0300

    Добавил сортировку слиянием

--- Lecture_3/Example4_algorithms.py
+++ Lecture_3/Example4_algorithms.py
@@ -21,7 +21,7 @@ print(quick_sort([7, 9, 11, 13, 15, 17, 58, 32, 1, 78, 61, 18]))
     
 # Ещё пример 
 def quicksort(array):
-    print(array)
+    # print(array)
     if len(array) < 2:
         return array
     else:
@@ -55,3 +55,33 @@ print(quicksort([10, 5, 2, 3]))
 
 # ● Сортировка слиянием
 
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[: mid]
+        right = nums[mid :]
+        merge_sort(left)
+        merge_sort(right)
+        i= j= k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+list1 = [7, 9, 11, 13, 15, 17, 58, 32, 1, 78, 61, 18]
+merge_sort(list1)
+print(list1)
