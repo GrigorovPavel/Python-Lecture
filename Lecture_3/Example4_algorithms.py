5ce436afaa5f83d385ab0897ae6fac0b5e816084
Pavel <grigorov_ps@mail.ru>
2024-04-11 23:45:55 +0300

    Добавил пояснение к Быстрой сортировке

--- Lecture_3/Example4_algorithms.py
+++ Lecture_3/Example4_algorithms.py
@@ -19,10 +19,7 @@ def quick_sort(arr):
         return quick_sort(less) + [pivot] + quick_sort(greater)
 print(quick_sort([7, 9, 11, 13, 15, 17, 58, 32, 1, 78, 61, 18]))
     
-
-
-# Пример 
-
+# Ещё пример 
 def quicksort(array):
     print(array)
     if len(array) < 2:
@@ -33,3 +30,28 @@ def quicksort(array):
         greater = [i for i in array[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
 print(quicksort([10, 5, 2, 3]))
+#  Пояснение
+"""
+● 1-е повторение рекурсии:
+○ array = [10, 5, 2, 3]
+○ pivot = 10
+○ less = [5, 2, 3]
+○ greater = []
+○ return quicksort([5, 2, 3]) + [10] + quicksort([])
+
+● 2-е повторение рекурсии:
+○ array = [5, 2, 3]
+○ pivot = 5
+○ less = [2, 3]
+○ greater = []
+○ return quicksort([2, 3]) + [5] + quicksort([]) # Важно! Не забывайте, что здесь помимо вызова рекурсии
+добавляется список [10]
+● 3-е повторение рекурсии:
+
+○ array = [2, 3]
+○ return [2, 3] # Сработал базовый случай рекурсии
+На этом работа рекурсии завершилась и итоговый список будет выглядеть таким образом: [2, 3] + [5] + [10] = [2, 3, 5,10]
+"""
+
+# ● Сортировка слиянием
+
